120ae2af9f83b0670ff8a29f5dbae1e3fabc6c05
OfficialZandrex <zandrex00@gmail.com>
2019-12-13 02:36:28 +0800

    - added sidebar - added nav - added css and js for bootstrap and jquery

---
+++ resources/views/layouts/side-navigation.blade.php
@@ -0,0 +1,12 @@
+<!-- Sidebar  -->
+<nav id="sidebar">
+  <div class="sidebar-header">
+    <h4 style="color: white;">Event Platform</h4>
+  </div>
+
+  <ul class="list-unstyled components">
+    <li>
+      <a href="#">Manage Events</a>
+    </li>
+  </ul>
+</nav>
