c7cded626f90d529fab29a9e3debe2edb8ce6c3e
Bryan-Poh <zandrex00@gmail.com>
2019-12-18 11:58:03 +0800

    Add files via upload

---
+++ 2019_12_11_032452_create_events_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('organizations_id');
+            $table->integer('register_id');
+            $table->integer('sessions_id');
+            $table->integer('ticket_id');
+            $table->integer('channel_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
