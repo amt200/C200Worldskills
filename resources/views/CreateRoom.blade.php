0c8b8f06c46561c9eb88777e58b16f8416e80818
theamandaaaaa <59019303+theamandaaaaa@users.noreply.github.com>
2019-12-29 21:56:37 +0800

    test

--- resources/views/CreateRoom.blade.php
+++ resources/views/CreateRoom.blade.php
@@ -2,7 +2,34 @@
 
 @section('content')
 <div class="panel-heading">
-    <h1>Create Room</h1>
+    <h1>Worldskills Conference 2019</h1>
+    <h5>August 23, 2019</h5>
+</div>
+
+<div class="panel-heading">
+    <br>
+    <h4>Create New Room</h4>
+    <br>
+    Name:<br>
+    <input type="text" size="50" name="channel_name" value=" " required/>
+    <br/>
+    <br>
+    Channel:<br>
+    <select>
+        <option value="channel_id" >Main</option>
+        <option value="channel_id">idk</option>
+    </select>
+    <br>
+    <Br>
+    Capacity:<br>
+    <input type="text" size="50" name="room_capacity" value=" " required/>
+    <br/>
+    <br>
+
+</div>
+<div>
+    <br>
+    <input type="submit" value="Save Room">
 </div>
 
 
