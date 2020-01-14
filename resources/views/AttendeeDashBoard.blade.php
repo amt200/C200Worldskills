9b9b6d4402a62b93ec932d1f2018259a535e2ba1
NGZIBIN <50009798+NGZIBIN@users.noreply.github.com>
2020-01-14 23:47:36 +0800

    AuthLogin

--- resources/views/AttendeeDashBoard.blade.php
+++ resources/views/AttendeeDashBoard.blade.php
@@ -10,7 +10,10 @@
                     <div class="card-body">
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
-
+                                @foreach($data as $i)
+                                    {{$i->event_name}}
+                                    {{$i->event_date}}<br>
+                                    @endforeach
                         </form>
                     </div>
                 </div>
